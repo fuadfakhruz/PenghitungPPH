aaa96a33c7bff2f94435667b820c916b69bea888
Muhammad Fuad Fakhruzzaki <153423917+fuadfakhruz@users.noreply.github.com>
2024-05-16 10:57:58 +0700

    Add files via upload

---
+++ src/main/java/com/fuad/penghitungpajakpenghasilan/PenghitungPajakPenghasilan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fuad.penghitungpajakpenghasilan;
+
+/**
+ *
+ * @author fuadz
+ */
+public class PenghitungPajakPenghasilan {
+    public static void main(String[] args) {
+        frameUtama obj = new frameUtama();
+        obj.show();
+    }
+}
